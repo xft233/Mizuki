2a14955ddb11d6baac2a69873fb2416c31eb4a4b
xft233 <106183235+xft233@users.noreply.github.com>
2025-09-29 17:29:13 +0800

    设置 site

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://mizuki.mysqil.com/",
+	site: "https://kokomi.qzz.io/",
 
 	base: "/",
 	trailingSlash: "always",
