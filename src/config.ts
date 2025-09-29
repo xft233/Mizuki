3a9209a35ce4b5a18133acaf7d71104dd90b8749
xft233 <106183235+xft233@users.noreply.github.com>
2025-09-29 12:27:17 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -257,7 +257,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar_kokomi.jpg", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+	avatar: "assets/images/avatar_kokomi.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
 	name: "Mizuki",
 	bio: "The world is big, you have to go and see",
 	links: [
